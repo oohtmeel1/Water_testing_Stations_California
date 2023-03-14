a4a9e98fcb84c963d70bedde37a52ad397312793
Agnes McFarlin <115196648+oohtmeel1@users.noreply.github.com>
2023-03-14 16:41:48 -0400

    Worked with data frames
    Used pl dataframes instead of Pandas to try something new, and worked with Folium Maps

--- FinalprojectinvolvingUSwaterquality.py
+++ FinalprojectinvolvingUSwaterquality.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 import polars as pl
+import folium
+#make sure to install ipyleaflet
 #https://catalog.data.gov/dataset/water-quality-data-0de37
 pd.set_option('display.max_columns', None)
 
@@ -13,7 +15,6 @@ lab_results = pl.read_csv(r"C:\Users\amcfa\gitfiles\Projects\MastersWork\Fundame
 
 # %%
 field_results
-
 # %%
 lab_results
 stations
@@ -33,8 +34,29 @@ stations.sample(n=10)
 stations.describe()
 # %%
 ## Results grouped by area sounds like a good place to start maaybe results limits, lat/long
-lab_results.select([pl.col('latitude'), pl.col('longitude'), pl.col('result'), pl.col('reporting_limit'), pl.col('units')])
+a = lab_results.select([pl.col('latitude'), pl.col('longitude'),pl.col('station_number'), pl.col('result'), pl.col('reporting_limit'), pl.col('units')])
 
 # %%
-stations.select([pl.col('latitude'), pl.col('longitude'), pl.col('station_number'),pl.col('sample_count')])
+b = stations.select([pl.col('latitude'), pl.col('longitude'), pl.col('station_number')])
+# %%
+c = field_results.select([pl.col('latitude'),pl.col('longitude'),pl.col('full_station_name'),pl.col('parameter'),pl.col('fdr_result'), pl.col('fdr_reporting_limit'),	pl.col('uns_name')])
+
+# %%
+
+
+# %%
+d = period_of_record.select([pl.col('latitude'),pl.col('longitude'),pl.col('sample_date_max'),pl.col('sample_date_min')])
+# %%
+##
+# %%
+a
+# %%
+b
+# %%
+c
+# %%
+d
+# %%
+m = folium.Map(location = [39.272938,-121.16])
+m
 # %%
